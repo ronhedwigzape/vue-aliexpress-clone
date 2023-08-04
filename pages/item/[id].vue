149f844d65dbd2b598ed8c730884040223a60f22
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-08-05 07:53:24 +0800

    feat: add trail of pics and functionality
    Signed-off-by: Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -4,15 +4,48 @@
             <div class="md:flex gap-4 justify-between mx-auto w-full">
                 <div class="md:w-[40%]">
                     <img
+                        v-if="currentImage"
                         class="rounded-lg object-fit"
-                        src="https://picsum.photos/id/212/800/800"
-                        alt="">
+                        :src="currentImage"
+                    >
+                    <div v-if="images[0] !== ''" class="flex items-center justify-center mt-2">
+                        <div v-for="image in images">
+                            <img
+                                @mouseover="currentImage = image"
+                                @click="currentImage = image"
+                                width="70"
+                                class="rounded-md object-fit border-[3px] cursor-pointer"
+                                :class="currentImage === image ? 'border-[#FF5353]' : ''"
+                                :src="image"
+                                alt="">
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </div>
     </MainLayout>
 </template>
 
 <script setup>
-    import MainLayout from "~/layouts/MainLayout.vue";
+import MainLayout from "~/layouts/MainLayout.vue";
+
+const currentImage = ref(null)
+
+onMounted(() => {
+    watchEffect(() => {
+        currentImage.value = 'https://picsum.photos/id/77/800/800'
+        images.value[0] = 'https://picsum.photos/id/77/800/800'
+    })
+})
+
+const images = ref([
+    '',
+    'https://picsum.photos/id/212/800/800',
+    'https://picsum.photos/id/233/800/800',
+    'https://picsum.photos/id/165/800/800',
+    'https://picsum.photos/id/99/800/800',
+    'https://picsum.photos/id/144/800/800',
+    'https://picsum.photos/id/214/800/800',
+])
 </script>
