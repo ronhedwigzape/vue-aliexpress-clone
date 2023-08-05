675d3955bafaa08021bea1097035e85f356dffb5
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-08-06 00:01:56 +0800

    chore([id].vue): finish styles
    Signed-off-by: Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -21,7 +21,55 @@
                         </div>
                     </div>
                 </div>
+                <div class="md:w-[60%] bg-white p-3 rounded-lg">
+                    <div v-if="true">
+                        <p class="mb-2">Title</p>
+                        <p class="font-light text-[12px] mb-2">Description Section</p>
+                    </div>
 
+                    <div class="flex items-center pt-1.5">
+                        <span class="h-4 min-w-4 rounded-full p-0.5 bg-[#FFD000] mr-2">
+                            <Icon name="material-symbols:star-rounded" class="-mt-[17px]" size="12"/>
+                        </span>
+                        <p class="text-[#FF5353]">Extra 5% off</p>
+                    </div>
+
+                    <div class="flex items-center justify-start my-2">
+                        <Icon name="ic:baseline-star" color="#FF5353"/>
+                        <Icon name="ic:baseline-star" color="#FF5353"/>
+                        <Icon name="ic:baseline-star" color="#FF5353"/>
+                        <Icon name="ic:baseline-star" color="#FF5353"/>
+                        <Icon name="ic:baseline-star" color="#FF5353"/>
+                        <span class="text-[13px] font-light ml-2">5 213 Reviews 1,000 + orders</span>
+                    </div>
+
+                    <div class="border-b"></div>
+
+                    <div class="flex items-center justify-start gap-2 my-2">
+                        <div class="text-xl font-bold">$ {{ priceComputed }}</div>
+                        <span class="bg-[#F5F5F5] border text-[#C08562] text-[9px] font-semibold px-1.5 rounded-sm">70% off</span>
+                    </div>
+
+                    <p class="text-[#009A66] text-xs font-semibold pt-1">
+                        Free 11-day delivery over $8.28
+                    </p>
+
+                    <p class="text-[#009A66] text-xs font-semibold pt-1">
+                        Free Shipping
+                    </p>
+
+                    <div class="py-2"></div>
+
+                    <button
+                        @click="addToCart()"
+                        :disabled="isInCart"
+                        class="px-6 py-2 rounded-lg text-white text-lg font-semibold bg-gradient-to-r from-[#FF851A] to-[#FFAC2C]"
+                    >
+                        <div v-if="isInCart">Is Added</div>
+                        <div v-else>Add to Cart</div>
+                    </button>
+
+                </div>
             </div>
         </div>
     </MainLayout>
@@ -29,6 +77,11 @@
 
 <script setup>
 import MainLayout from "~/layouts/MainLayout.vue";
+import { useUserStore } from "~/stores/user";
+import {useRoute} from "#app";
+const userStore = useUserStore()
+
+const route = useRoute();
 
 const currentImage = ref(null)
 
@@ -37,6 +90,10 @@ onMounted(() => {
         currentImage.value = 'https://picsum.photos/id/77/800/800'
         images.value[0] = 'https://picsum.photos/id/77/800/800'
     })
+})
+
+const priceComputed = computed(() => {
+    return '26.40'
 })
 
 const images = ref([
@@ -48,4 +105,19 @@ const images = ref([
     'https://picsum.photos/id/144/800/800',
     'https://picsum.photos/id/214/800/800',
 ])
+
+const addToCart = () => {
+    alert('ADDED')
+}
+
+const isInCart = computed(() => {
+    let res = false
+    userStore.cart.forEach((product) => {
+        if(route.params.id === product.id) {
+            res = true
+        }
+    })
+    return res;
+})
+
 </script>
