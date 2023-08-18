ce9157e3ba3dfd410bf552563b286e2473dee403
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-19 01:00:24 +0800

    feat: add TextInput component

---
+++ components/TextInput.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <client-only>
+            <input
+                :placeholder="placeholder"
+                class="w-full bg-white text-gray-800 border text-sm border-[#EFF0EB] rounded-lg p-3 placeholder-gray-500 focus:outline-none"
+                @focus="isFocused = true"
+                @blur="isFocused = false"
+                :class="{
+                    'border-gray-900': isFocused,
+                    'border-red-500': error
+                }"
+                :type="inputType"
+                v-model="inputComputed"
+                autocomplete="off"
+            />
+        </client-only>
+    </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['update:input'])
+
+const props = defineProps(['input', 'placeholder', 'max', 'inputType', 'error'])
+const { input, placeholder, max, inputType, error } = toRefs(props)
+
+let isFocused = ref(false)
+
+const inputComputed = computed({
+    get: () => input.value,
+    set: (val) => emit('update:input', val)
+})
+</script>
+
+
