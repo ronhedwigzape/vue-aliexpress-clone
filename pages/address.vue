9ea0850ae7f65092d99654184ea748550530da87
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-09-16 00:12:26 +0800

    feat: add button and initialize scripts

--- pages/address.vue
+++ pages/address.vue
@@ -44,8 +44,18 @@
                         :error="error && error.type == 'country' ? error.message : ''"
                     />
 
-                    <button>
-
+                    <button
+                      :disabled="isWorking"
+                      type="submit"
+                      class="mt-6 bg-gradient-to-r from-[#FE630C] to-[#FF3200] w-full text-white text-[21px] font-semibold p-1.5 rounded-full"
+                    >
+                      <div v-if="!isWorking">Update Address</div>
+                      <Icon
+                        v-else
+                        name="eos-icons:loading"
+                        size="25"
+                        class="mr-2"
+                      />
                     </button>
                 </form>
             </div>
@@ -56,5 +66,60 @@
 <script setup>
 
 import MainLayout from "~/layouts/MainLayout.vue";
+import { useUserStore } from "~/stores/user";
+const userStore = useUserStore();
+
+let contactName = ref(null);
+let address = ref(null);
+let zipCode = ref(null);
+let city = ref(null);
+let country = ref(null);
+let currentAddress = ref(null);
+let isUpdate = ref(null);
+let isWorking = ref(null);
+let error = ref(null);
+
+watchEffect(() => {
+
+});
+
+const submit = async () => {
+  isWorking.value = true;
+  error.value = null
+
+  if(!contactName.value) {
+    error.value = {
+      type: 'contactName',
+      message: 'A contact name is required'
+    }
+  } else if (!address.value) {
+    error.value = {
+      type: 'address',
+      message: 'An address is required'
+    }
+  } else if (!zipCode.value) {
+    error.value = {
+      type: 'zipCode',
+      message: 'A zip code is required'
+    }
+  } else if (!city.value) {
+    error.value = {
+      type: 'city',
+      message: 'A city is required'
+    }
+  } else if (!country.value) {
+    error.value = {
+      type: 'country',
+      message: 'A country is required'
+    }
+  }
+
+  if (error.value) {
+    isWorking.value = false
+
+  }
+
+  // add here
+}
 </script>
 
